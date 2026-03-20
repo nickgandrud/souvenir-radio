19753e25b771fd8166d53d91098176a031720d8b
Nick Gandrud <nickgandrud@Nicks-MacBook-Air.local>
2026-03-20 10:21:39 -0500

    Update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
-const AZURACAST_BASE = "http://104.236.123.205";
+const AZURACAST_BASE = "Removed IP";
 const STATION_SHORTCODE = "test_";
 
-const FALLBACK_STREAM_URL = "http://104.236.123.205/listen/test_/radio.mp3";
+const FALLBACK_STREAM_URL = "Removed IP";
 
 // AzuraCast static Now Playing JSON:
 // http(s)://host/api/nowplaying_static/<station_shortcode>.json
